e23e1944602fc29cab96a6e60b1a33ed26bb1dcd
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-03-31 00:30:51 +0600

    fetched data and created cards

---
+++ src/components/ModelCard.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import { FaFireAlt } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
+
+const ModelCard = ({ model }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className=" border border-zinc-400 shadow-md rounded-lg">
+      <div className="flex justify-center items-center h-44 w-full bg-gray-50 p-8 rounded-lg relative group">
+        <img
+          className="max-h-full max-w-full object-contain transform transition-transform duration-500 ease-in-out group-hover:scale-120"
+          src={model.image}
+          alt=""
+        />
+        <div
+          className={`absolute top-3 right-3 badge ${
+            model.status === "popular"
+              ? "badge-secondary"
+              : model.status === "favourite"
+                ? "badge-warning"
+                : "badge-success"
+          }`}
+        >
+          {model.status === "popular" ? (
+            <FaFireAlt />
+          ) : model.status === "favourite" ? (
+            <FaHeart />
+          ) : (
+            <CiStar />
+          )}
+          {model.status}
+        </div>
+      </div>
+      <div className="p-6 space-y-3">
+        <h1>{model.title}</h1>
+        <p
+          className={`text-gray-600 text-sm mt-2 ${isExpanded ? "" : "line-clamp-1"}`}
+        >
+          {" "}
+          {model.description}{" "}
+        </p>{" "}
+        <button
+          onClick={() => setIsExpanded(!isExpanded)}
+          className="text-blue-500 text-xs font-bold mt-1 hover:underline"
+        >
+          {" "}
+          {isExpanded ? "Show Less" : "Read More"}{" "}
+        </button>
+        <p>
+          {model.price === 0 ? (
+            <span className="font-bold text-4xl text-green-500">Free</span>
+          ) : (
+            <span>
+              <span className="font-bold text-4xl">${model.price}</span>
+              /month
+            </span>
+          )}
+        </p>
+        <button className="btn btn-secondary w-full rounded-lg">
+          Subscribe Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ModelCard;
--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -1,10 +1,26 @@
 import React, { use } from "react";
+import ModelCard from "./ModelCard";
 
 const Models = ({ modelPromise }) => {
   const modelsArray = use(modelPromise);
-  console.log(modelsArray);
 
-  return <div></div>;
+  return (
+    <>
+      <div className="max-w-6xl mx-auto mb-8">
+        <h1 className="font-bold text-5xl text-center mb-5">
+          Choose Your AI Model
+        </h1>
+        <p className="font-medium text-zinc-400 text-3xl text-center mb-10">
+          One subscription gives you access to all frontier AI models
+        </p>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-10 items-start">
+          {modelsArray.map((model) => (
+            <ModelCard key={model.id} model={model}></ModelCard>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Models;
